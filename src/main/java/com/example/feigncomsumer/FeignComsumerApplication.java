85f3206beee6689ed706385ef1a95a095b6f0933
lilin <1142114078@qq.com>
2019-02-22 16:48:35 +0800

    first commit

---
+++ src/main/java/com/example/feigncomsumer/FeignComsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.feigncomsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignComsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignComsumerApplication.class, args);
+    }
+
+}
